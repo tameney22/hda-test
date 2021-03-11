3f93ee59c474c30a3a6f9bb4ab3d29184ab59fa2
Yoseph Tamene <tameney22@mail.wlu.edu>
2021-03-10 23:44:51 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage'
+import DigitalEdition from './pages/DigitalEdition';
+import Navigation from './components/Navigation'
+import TestPage from './pages/TestPage';
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <div>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/test" component={DigitalEdition} />
+          <Route path="/test2" component={TestPage} />
+          <Route path="/about" component={AboutPage} />
+        </Switch>
+      </div>
+      {/* footer */}
     </div>
-  );
+  </Router>);
 }
 
 export default App;
---
+++ src/pages/DigitalEdition.js
@@ -0,0 +1,135 @@
+import { TEIRender, TEIRoute } from 'react-teirouter'
+import BXML from '../teis/b.xml'
+import axios from 'axios';
+import { useState } from 'react';
+import { getChildrenDeep } from 'react-nanny'
+
+const Line = (props) => {
+    // let lineNum = props.teiDomElement.getAttribute('n')
+    // console.log("CALLED:", lineNum)
+    return (
+        <p>{props.text}</p>
+    )
+}
+
+const LaisseTextOld = (props) => {
+    return (
+        <div>{props.text}</div>
+    )
+}
+
+const LaisseText = (props) => {
+    let children = props.laisse.children
+    let lines = Array.from(children).map((line, i) => {
+        return <Line key={i} text={line.textContent} line={line} />
+    });
+    return (
+        <>
+            <p>{children.length} Lines</p>
+            {/* <div>{props.text}</div> */}
+            {lines}
+        </>
+    )
+}
+
+// variable for only showing the some of the Laisse for now
+var justSome = 5
+var count = 0
+const LaisseTest = (props) => {
+    if (count < justSome) {
+        count++
+        let children = Array.from(props.teiDomElement.children)
+        console.log("LINES:", children.length)
+        let lines = children.filter(line => line.textContent).map((line, i) => {
+            return <Line key={i} text={line.textContent} />
+        });
+        return (
+            <div>{lines}</div>
+        )
+    } else {
+        return (<div></div>)
+    }
+}
+
+const Laisse = (props) => {
+    return (
+        <div>{props.children}</div>
+    )
+}
+
+const Title = (props) => {
+    let manuName = props.teiDomElement.textContent;
+    return (
+        <h2>{manuName}</h2>
+    )
+}
+
+const BodyOld = (props) => {
+    let children = props.teiDomElement.children //.filter(elem => elem.nodeName === "tei-milestone").length
+    // console.log(children)
+    let content = Array.from(children).map((lg, i) => {
+        return <LaisseText key={i} text={lg.textContent} laisse={lg} />
+    });
+    // let milestones = children.querySelectorAll('tei-milestone')
+    // console.log(milestones)
+    return (
+        <>
+            <h3>{children.length} elements found</h3>
+            <div>{content}</div>
+        </>
+    )
+}
+
+const Body = (props) => {
+    let children = props.teiDomElement.children //.filter(elem => elem.nodeName === "tei-milestone").length
+    // console.log(children)
+    let content = Array.from(children).map((lg, i) => {
+        return <LaisseText key={i} text={lg.textContent} laisse={lg} />
+    });
+    // let milestones = children.querySelectorAll('tei-milestone')
+    // console.log(milestones)
+    return (
+        <>
+            <h3>{children.length} elements found</h3>
+            <div>{content[0]}</div>
+        </>
+    )
+}
+
+const Milestone = (props) => {
+    let mile = props.teiDomElement.getAttribute('n')
+    // let parent = props.teiDomElement.parentNode
+    // console.log(parent)
+    return (
+        <p>{mile}</p>
+    )
+}
+
+const DigitalEdition = () => {
+    const [tei, setTei] = useState({ data: null, ready: false })
+
+    axios.get(BXML, {
+        "Content-Type": "application/xml; charset=utf-8"
+    }).then((response) => {
+        setTei({ data: response.data, ready: true })
+        // console.log(tei)
+    })
+
+    return (
+        <div>
+            <h1>B.XML</h1>
+            {tei.ready ?
+                <TEIRender teiData={tei.data} path={tei.data}>
+                    {/* <TEIRoute el="tei-title" component={Title} /> */}
+                    {/* <TEIRoute el='tei-l' component={Line} /> */}
+                    {/* <TEIRoute el='tei-lg' component={LaisseTest} /> */}
+                    <TEIRoute el='tei-body' component={Body} />
+                    {/* <TEIRoute el='tei-milestone' component={Milestone} /> */}
+                </TEIRender>
+                : <h2>Loading. . .</h2>}
+        </div>
+    );
+
+}
+
+export default DigitalEdition;
