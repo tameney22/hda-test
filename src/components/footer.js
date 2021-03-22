01428b0358010320458a20fd065e4a80cd5df27a
akhaledan <acr9977@gmail.com>
2021-03-22 17:53:50 -0400

    began integrating the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,32 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className = "footer">
-            <p>TEST FOOTER</p>
-        </div>
+        <footer role="contentinfo" class = "footer">
+            <div class = "footer-row">
+                <div class = "footer-column-1">
+
+                </div>
+                <div class = "footer-column-1">
+                    
+                </div>
+                <div class = "footer-column-1">
+                    
+                </div>
+                ::after
+            </div>
+            <div class = "footer-row">
+                <div class = "footer-column-2">
+
+                </div>
+                <div class = "footer-column-2">
+    
+                </div>
+                <div class = "footer-column-2">
+    
+                </div>
+                ::after
+            </div>
+        </footer>
     );
 }
 
